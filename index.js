68667bad3d86ed3f0128dc4e78ff0d58c2709751
natthasit <674259007@webmail.npru.ac.th>
2026-09-17 16:26:39 +0700

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-dotenv .config();
-const PORS = process.env.BACKEND_PORT;
-
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.BACKEND_PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req, res) => {
-    return
-    res.status(200)
-    .send("<h>welcom to estful API for Product Management</h>")
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .send("<h>Welcom to Restful API for Product Management App</h>");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on: http://localhost:${PORT}`);
+  console.log(`Server is running on: http://localhost:${PORT}`);
 });
